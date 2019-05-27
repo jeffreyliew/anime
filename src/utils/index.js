cb5bf4a174e6a2862cfb98e53eb22b0cffc678ef
Jeffrey Liew <jeffreyliew95@gmail.com>
2019-05-28 00:15:29 +0200

    constants for async data fetching

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,19 @@
-export function determineOrder(orderState) {
-  if (orderState === "ascending") {
+export function determineOrder(order) {
+  if (order === "ascending") {
     return (a, b) => a.score - b.score;
-  } else if (orderState === "descending") {
+  } else if (order === "descending") {
     return (a, b) => b.score - a.score;
   }
 }
+
+export const DAY = [
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday",
+  "sunday"
+];
+
+export const URL = "https://api.jikan.moe/v3/schedule";
