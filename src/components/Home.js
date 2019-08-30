4b02fc455f0a80609f77a2bb3b5ed4f8dba33455
Jeffrey Liew <jeffreyliew95@gmail.com>
2019-08-30 16:34:13 +0200

    change background opacity and enter button

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,12 +5,12 @@ import { Link } from "react-router-dom";
 export const Home = props => {
   return (
     <div className="home">
-      <div className="home__enter-button">
-        <Link to="/anime" style={{ textDecoration: "none", outline: "none" }}>
-          <span>Enter</span>
-        </Link>
-      </div>
       <div className="home__overlay" />
+      <Link to="/anime" style={{ textDecoration: "none", outline: "none" }}>
+        <div className="home__enter-button">
+          <span>Enter</span>
+        </div>
+      </Link>
     </div>
   );
 };
