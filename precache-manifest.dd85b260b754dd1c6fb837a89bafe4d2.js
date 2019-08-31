ec3ac11fc6921d5b27c7b1ed63caf4316f044e47
Jeffrey Liew <jeffreyliew95@gmail.com>
2019-08-31 17:43:55 +0200

    Updates

---
+++ precache-manifest.dd85b260b754dd1c6fb837a89bafe4d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4758bbd71ea1f50a199e3336aaff7dc2",
+    "url": "/anime/index.html"
+  },
+  {
+    "revision": "13eaf44e73bef4f3581a",
+    "url": "/anime/static/css/main.3abef4a5.chunk.css"
+  },
+  {
+    "revision": "19c540884c1a87d3982d",
+    "url": "/anime/static/js/2.d3d26f75.chunk.js"
+  },
+  {
+    "revision": "13eaf44e73bef4f3581a",
+    "url": "/anime/static/js/main.ca62c59e.chunk.js"
+  },
+  {
+    "revision": "83c1486d68fd990a0f18",
+    "url": "/anime/static/js/runtime~main.a0709b55.js"
+  }
+]);
