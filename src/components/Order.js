ac6cd36328ef5bc7cf456fa628bf35e74119e05d
Jeffrey Liew <jeffreyliew95@gmail.com>
2019-09-02 23:31:40 +0200

    detect click outside of dropdown menu

--- src/components/Order.js
+++ src/components/Order.js
@@ -8,25 +8,50 @@ class Order extends Component {
     super(props);
     this.onOrderChange = this.onOrderChange.bind(this);
     this.toggleHide = this.toggleHide.bind(this);
+    this.handleOutsideClick = this.handleOutsideClick.bind(this);
     this.state = {
       hidden: true
     };
   }
 
+  componentDidMount() {
+    document.addEventListener("mousedown", this.handleOutsideClick);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.handleOutsideClick);
+  }
+
   // invoke onClick to set order
   onOrderChange(e) {
+    e.stopPropagation();
     this.props.onClick(e.target.textContent.replace(/\s/g, ""));
   }
 
-  toggleHide() {
+  toggleHide(e) {
+    e.stopPropagation();
     this.setState({ hidden: !this.state.hidden });
+  }
+
+  handleOutsideClick(e) {
+    e.stopPropagation();
+    if (
+      this.dropdown === e.target ||
+      e.target.className.indexOf("order-dropdown__btn") !== -1
+    ) {
+      return;
+    }
+    this.setState({ hidden: true });
   }
 
   render() {
     const { order } = this.props;
     return (
       <div className="order-dropdown" onClick={this.toggleHide}>
-        <i className="fas fa-sort fa-2x" />
+        <i
+          className="fas fa-sort fa-2x"
+          ref={dropdown => (this.dropdown = dropdown)}
+        />
         <div
           className="order-dropdown__content"
           style={this.state.hidden ? {} : { display: "block" }}
